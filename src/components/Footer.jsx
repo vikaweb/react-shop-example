354fbd7651929197d30c1b9badc1d80ff918b59f
Viktoriya Tochiyeva <06-10-1993@mail.ru>
2025-03-12 14:52:33 +0500

    pravki

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,10 @@ function Footer() {
       <div className="footer-copyright">
         <div className="container">
           © {new Date().getFullYear()} Copyright Text
-          <a className="grey-text text-lighten-4 right" href="#!">
+          <a
+            className="grey-text text-lighten-4 right"
+            target="_blank"
+            href="https://github.com/vikaweb/react-shop-example">
             Repo
           </a>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,16 @@ function Header() {
   return (
     <nav className="green darken-1">
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo">
+        <a href="/" className="brand-logo">
           React Movies
         </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <a target="_blank" href="https://github.com/vikaweb/react-shop-example">
+              Repo
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
